398475884bc68312792498ffd50e5a25ca9ea439
Frainer Encarnación <fraineralex2001@gmail.com>
2023-03-03 12:42:34 -0400

    [ADD] add documentation about handleError.js file

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -2,6 +2,7 @@
 /* This is a function that is exported to the app.js file. It is a function that is called when the
 user tries to access a page that does not exist. */
 
+/* Exporting a function that is called when the user tries to access a page that does not exist. */
 module.exports = (error, request,response,next) =>{
     console.error(error)
 
